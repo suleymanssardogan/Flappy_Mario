7ec001a978e75d824a345ef05841409375c56cd2
suleymanssardogan <sardogansuleyman04@gmail.com>
2024-11-24 10:51:04 +0300

    Flappy_Mario
    C# ile Masaüstü Oyun

---
+++ Flappy_Mario/Flappy_Mario/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Flappy_Mario
+{
+    partial class Flappy_Mario
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+           #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Flappy_Mario));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxup = new System.Windows.Forms.PictureBox();
+            this.pictureBoxdown = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmario = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxdown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(59, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // pictureBoxup
+            // 
+            this.pictureBoxup.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxup.Image")));
+            this.pictureBoxup.Location = new System.Drawing.Point(490, 12);
+            this.pictureBoxup.Name = "pictureBoxup";
+            this.pictureBoxup.Size = new System.Drawing.Size(180, 203);
+            this.pictureBoxup.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxup.TabIndex = 2;
+            this.pictureBoxup.TabStop = false;
+            // 
+            // pictureBoxdown
+            // 
+            this.pictureBoxdown.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxdown.Image")));
+            this.pictureBoxdown.Location = new System.Drawing.Point(1068, 420);
+            this.pictureBoxdown.Name = "pictureBoxdown";
+            this.pictureBoxdown.Size = new System.Drawing.Size(196, 221);
+            this.pictureBoxdown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxdown.TabIndex = 1;
+            this.pictureBoxdown.TabStop = false;
+            // 
+            // pictureBoxmario
+            // 
+            this.pictureBoxmario.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmario.Image")));
+            this.pictureBoxmario.Location = new System.Drawing.Point(83, 335);
+            this.pictureBoxmario.Name = "pictureBoxmario";
+            this.pictureBoxmario.Size = new System.Drawing.Size(75, 70);
+            this.pictureBoxmario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmario.TabIndex = 0;
+            this.pictureBoxmario.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(320, 315);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Flappy_Mario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1444, 691);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBoxup);
+            this.Controls.Add(this.pictureBoxdown);
+            this.Controls.Add(this.pictureBoxmario);
+            this.Name = "Flappy_Mario";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Flappy_Mario_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Flappy_Mario_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxdown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBoxmario;
+        private System.Windows.Forms.PictureBox pictureBoxdown;
+        private System.Windows.Forms.PictureBox pictureBoxup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Flappy_Mario/Flappy_Mario/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Mario
+{
+    public partial class Flappy_Mario : Form
+    {
+        public Flappy_Mario()
+        {
+            InitializeComponent();
+        }
+
+        //Engellerin  geliş hızı
+        int pipe_speed = 28;
+        //space tuşunun bırakınca karakterin ne kadar aşağı gideceği değer
+        int gravity = 5;
+        //oyuncu skoru
+        int score = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+           //boruların sola doğru hareketi
+            pictureBoxdown.Left -= pipe_speed;
+            pictureBoxup.Left -= pipe_speed;
+
+            //skor güncelleme
+            label2.Text = "Score: "+score;
+
+            //Mario düşme efekti olarak belirlediğimiz gravity değeri ekleniyor
+            pictureBoxmario.Top += gravity;
+
+            // Borular ekranın dışına çıktığında pozisyonlarını sıfırla ve skoru artır
+            if (pictureBoxdown.Left < -150)
+            {
+                //boruyu sa ğa sıfırlar
+                pictureBoxdown.Left = 800;
+                score++;
+                
+            }
+
+            if (pictureBoxup.Left < -180)
+            {
+                //Boruyu sağa sıfırlar
+                pictureBoxup.Left = 950;
+                score++;
+                
+            }
+            // Mario ile borular ya da ekran sınırları (üst/alt) arasında çarpışma kontrolü
+            if (pictureBoxmario.Bounds.IntersectsWith(pictureBoxdown.Bounds) ||
+    pictureBoxmario.Bounds.IntersectsWith(pictureBoxup.Bounds) ||
+    pictureBoxmario.Top < 0 || pictureBoxmario.Bottom > ClientSize.Height)
+            {
+                //Çarpışma olursa GameOver fonksiyonu çağrılır
+                GameOver();
+            }
+
+
+            if (score >= 20)
+            {
+                pictureBox1.Visible = true;
+                GameOver();
+            }
+
+
+        }
+
+        private void Flappy_Mario_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();                    
+                
+        }
+     
+
+       
+
+        private void Flappy_Mario_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Space tuşunu atama
+            if(e.KeyCode == Keys.Space)
+            {
+                //Marioyu Yukarı hareket ettirme
+                pictureBoxmario.Top -= 60;
+            }
+        }
+        private void GameOver()
+        {
+           
+            timer1.Stop();
+            label2.Text += " - Game Over!";
+            MessageBox.Show("Game Over! Your score is: " + score);
+            //score sıfırlanır
+            score = 0;
+            pictureBox1.Visible = false;
+
+            //Marioyu ortalar yeni oyuna hazrı hale getirir
+            pictureBoxmario.Top = ClientSize.Height / 2;
+
+            //Boruları yeni oyuna hazır hale getirir sağa yanaştırır
+            pictureBoxdown.Left = 800;
+            pictureBoxup.Left = 950;
+
+            //timer yeniden başlatılır
+            timer1.Start();
+        }
+    }
+}
